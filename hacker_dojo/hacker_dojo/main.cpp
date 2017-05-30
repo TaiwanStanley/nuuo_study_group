ca4c00f88b86b209ccf36d913169a6caae407265
stanley_chen <stanley_chen@nuuo.com>
2017-05-30 17:18:04 +0800

    Refacotry - using TelephoneNumberForm class to replace the output logic

--- hacker_dojo/hacker_dojo/main.cpp
+++ hacker_dojo/hacker_dojo/main.cpp
@@ -3,6 +3,7 @@
 #include <iomanip>
 #include <map>
 #include "StandardNumberCompiler.h"
+#include "TelephoneNumberForm.h"
 using namespace std;
 
 inline int GetOneLineToInt()
@@ -15,42 +16,23 @@ inline int GetOneLineToInt()
 int main(void)
 {
     int datasets = GetOneLineToInt();
+
     while (datasets--)
     {
+        CTelephoneNumberForm teleform;
+
         cin.ignore(100000, '\n');
         int telephone_number_list = GetOneLineToInt();
-        map<string, unsigned int> result_map;
-        bool has_duplicates = false;
 
         for (int i = 0; i < telephone_number_list; i++)
         {
             string memorable_telephone_number;
             getline(cin, memorable_telephone_number);
+            teleform.ToStandardForm(memorable_telephone_number);
+        }
 
-            string stardard_telphone_number = CStandardNumberCompiler::ToStandardForm(memorable_telephone_number);
-            map<string, unsigned int>::iterator it = result_map.find(stardard_telphone_number);
-            if (it != result_map.end())
-            {
-                it->second++;
-                has_duplicates = true;
-            }
-            else
-            {
-                result_map[stardard_telphone_number] = 1;
-            }
-        }
-        map<string, unsigned int>::iterator it = result_map.begin();
-        for (; it != result_map.end(); it++)
-        {
-            if (it->second > 1)
-            {
-                cout << it->first << " " << it->second << endl;
-            }
-        }
-        if (!has_duplicates)
-        {
-            cout << "No duplicates." << endl;
-        }
+        teleform.PrintDuplicatesStandardFormNumber();
+        teleform.HasDuplicatesNum();
 
         if (datasets != 0)
         {
