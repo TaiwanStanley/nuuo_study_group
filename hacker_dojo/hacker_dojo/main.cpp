4f31e216b39a59f9074f43f29590374d07a6ca02
stanley_chen <stanley_chen@nuuo.com>
2017-07-03 23:35:23 +0800

    read test cases

---
+++ hacker_dojo/hacker_dojo/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+void test_case()
+{
+
+}
+
+int main()
+{
+    int cases = 0;
+    cin >> cases;
+    while (cases--)
+    {
+        test_case();
+    }
+
+    return 0;
+}
