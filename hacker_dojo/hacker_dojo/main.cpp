734c6d0641e609562e1453851263631c260c996a
stanley_chen <stanley_chen@nuuo.com>
2017-08-14 01:33:52 +0800

    implement - just a function call, not including classes

--- hacker_dojo/hacker_dojo/main.cpp
+++ hacker_dojo/hacker_dojo/main.cpp
@@ -1,18 +1,73 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <stdexcept>
 using namespace std;
 
-void test_case(vector<string>& minesweeper, const vector<string>& touched_pos)
+bool test_case(vector<string>& minesweeper, const vector<string>& touched_pos)
 {
+    bool got_mine = false;
 
+    for (size_t rows = 0; rows < minesweeper.size(); rows++)
+    {
+        for (size_t columns = 0; columns < minesweeper[rows].size(); columns++)
+        {
+            if (touched_pos[rows][columns] == 'x')
+            {
+                if (minesweeper[rows][columns] == '*')
+                {
+                    // bomb
+                    got_mine = true;
+                }
+                else
+                {
+                    // do mask (3*3)
+                    char nighborhood_mines = '0';
+                    for (int i = -1; i <= 1; i++)
+                    {
+                        for (int j = -1; j <= 1; j++)
+                        {
+                            try
+                            {
+                                if (minesweeper.at(rows + i).at(columns + j) == '*')
+                                {
+                                    nighborhood_mines = nighborhood_mines + 1;
+                                }
+                            }
+                            catch (const out_of_range& oor)
+                            {
+                            }
+                        }
+                    }
+                    minesweeper[rows][columns] = nighborhood_mines;
+                }
+            }
+        }
+    }
+    return got_mine;
 }
 
-void output(const vector<string>& minesweeper)
+void output(const vector<string>& minesweeper, bool got_mine, size_t testcase)
 {
     for (auto v : minesweeper)
     {
-        cout << v << endl;
+        if (got_mine)
+        {
+            cout << v;
+        }
+        else
+        {
+            for (auto s : v)
+            {
+                s == '*' ? cout << '.' : cout << s;
+            }
+        }
+        cout << endl;
+    }
+
+    if (testcase > 0)
+    {
+        cout << endl;
     }
 }
 
@@ -43,12 +98,10 @@ int main()
             touched_pos.push_back(str);
         }
 
-        test_case(minesweeper, touched_pos);
+        bool got_mine = test_case(minesweeper, touched_pos);
 
-        output(minesweeper);
+        output(minesweeper, got_mine, testcase);
     }
 
-
-    system("pause");
     return 0;
 }
