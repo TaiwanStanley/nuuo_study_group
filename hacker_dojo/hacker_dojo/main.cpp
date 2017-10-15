31edcb9ac2bf0c5171168d82e2503ea3da51c94b
stanley_chen <stanley_chen@nuuo.com>
2017-10-15 21:14:20 +0800

    read one line to know how many the number of test cases

---
+++ hacker_dojo/hacker_dojo/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void testcases(const string &fen_str)
+{
+
+}
+
+int main()
+{
+    string fen_str;
+    while (getline(cin, fen_str))testcases(fen_str);
+    return 0;
+}
